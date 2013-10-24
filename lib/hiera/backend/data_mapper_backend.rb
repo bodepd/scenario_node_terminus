2f93ba3090ba80107af1bbde4b6519241fb6c751
Dan Bode <bodepd@gmail.com>
2013-10-23 23:17:54 -0700

    Dev work
    saves some dev work:
    
    - decouples hiera interpolation from hiera lookup
    - adds placeholders for class/role override code

--- lib/hiera/backend/data_mapper_backend.rb
+++ lib/hiera/backend/data_mapper_backend.rb
@@ -3,13 +3,15 @@ class Hiera
     class Data_mapper_backend
 
       def lookup(key, scope, order_override, resolution_type)
+        require 'puppet/bodepd/scenario_helper'
         unless data_bindings = scope['node_data_bindings']
           raise(
             Exception,
             "expected variable: #{node_data_bindings} to be set from node terminus"
           )
         end
-        data_bindings[key]
+        include Puppet::Bodepd::ScenerioHelper
+        interpolate_data(data_bindings[key], scope)
       end
 
     end
